bf47e4970777b30f76197103d05fc05fb9159d06
adw-ith <adwaithraj45654@gmail.com>
2024-09-22 22:06:57 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/T24.png" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
